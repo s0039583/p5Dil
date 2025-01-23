1a26d15d82f7939fbfe7dbeff3d92f598393d396
s0039583 <s0039583@wyke.ac.uk>
2025-01-23 19:33:08 +0000

    Update sketch.js

--- DIL 1/Challenge 3/sketch.js
+++ DIL 1/Challenge 3/sketch.js
@@ -1,22 +1,17 @@
-let pipe1, pipe2, bird, floor;
+let block, floor;
 
 function setup() {
   x = 600;
   y = 400;
-  createCanvas(x, y);
+  createCanvas(x,y);
+  displayMode('centered');
   world.gravity.y = 10;
-floor = new Sprite(x/2,y,x,2,"s")
-  pipe1 = new Sprite(150,50,25,100,"s");
-  pipe2 = new Sprite(150,300,25,200,"s");
-  pipe1.color = "green";
-  pipe2.color = "green";
-
-  bird = new Sprite(0,y/2,50,50,"d");
-  bird.image = "🐥"
-  bird.vel.x = 2;
-
+  floor = new Sprite(x/2,y,x,2,"s");
 }
 
 function draw() {
-  background('skyblue');
-}
+  background(0);
+  if(mouse.pressed()){
+    block = new Sprite(mouse.x,mouse.y);
+  }
+ }
