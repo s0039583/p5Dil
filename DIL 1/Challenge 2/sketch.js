fb15233d138284800d6992d353e2d3ce33d3a66e
s0039583 <s0039583@wyke.ac.uk>
2025-01-23 18:31:17 +0000

    Update sketch.js

--- DIL 1/Challenge 2/sketch.js
+++ DIL 1/Challenge 2/sketch.js
@@ -1,30 +1,44 @@
+let dvd;
+
 function setup() {
   x = 600;
   y = 400;
   createCanvas(x,y);
   displayMode('centered');
 
-  ceiling = new Sprite(x/2, 0, x, 2, "s");
-  bottom = new Sprite(x/2, y, x, 2, "s");
-  left = new Sprite(0, y/2, 2, y, "s");
-  right = new Sprite(x, y/2, 2, y, "s");
-  ceiling.color = "black";
-  bottom.color = "black";
-  left.color = "black";
-  right.color = "black";
+  //ceiling = new Sprite(x/2, 0, x, 2, "s");
+  //bottom = new Sprite(x/2, y, x, 2, "s");
+  //left = new Sprite(0, y/2, 2, y, "s");
+  //right = new Sprite(x, y/2, 2, y, "s");
 
-  logo = new Sprite(x/2, y/2, 100, 75, "d");
-  logo.text = "DVD";
-  logo.vel.y = 1;
-  logo.vel.x = 1;
-  logo.direction.x = 1;
-  logo.direction.y = 1;
-  //logo.bounciness = 1;
+  dvd = new Sprite();
+  dvd.text = "DVD";
+  dvd.w = 120;
+  dvd.h = 80;
+  dvd.vel.y = 3;
+  dvd.vel.x = 1;
   logo.rotationLock = true;
 }
 
 function draw() {
   background(0);
-
-
+  
+  switch(true){
+    case dvd.x >= x-60:
+    dvd.vel.x *= -1
+    dvd.color = color(random(255),random(255),random(255)) 
+    break;
+    case dvd.x <= 60: 
+    dvd.vel.x *= -1 
+    dvd.color = color(random(255),random(255),random(255)) 
+    break;
+    case dvd.y >= y-40: 
+    dvd.vel.y *= -1 
+    dvd.color = color(random(255),random(255),random(255)) 
+    break;
+    case dvd.y <= 40: 
+    dvd.vel.y *= -1 
+    dvd.color = color(random(255),random(255),random(255))
+    break;
+    }
 }
